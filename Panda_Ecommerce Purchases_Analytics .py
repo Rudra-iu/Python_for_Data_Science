280c0f184a9e0fa5a410abf04220487875b9d25b
Sourav Mukherjee <32021944+souravgithubpage@users.noreply.github.com>
2022-12-11 22:41:19 -0500

    Update Panda_Ecommerce Purchases_Analytics .py

--- Panda_Ecommerce Purchases_Analytics .py
+++ Panda_Ecommerce Purchases_Analytics .py
@@ -7,11 +7,6 @@
 # ___
 # # Ecommerce Purchases Exercise
 # 
-# In this Exercise you will be given some Fake Data about some purchases done through Amazon! Just go ahead and follow the directions and try your best to answer the questions and complete the tasks. Feel free to reference the solutions. Most of the tasks can be solved in different ways. For the most part, the questions get progressively harder.
-# 
-# Please excuse anything that doesn't make "Real-World" sense in the dataframe, all the data is fake and made-up.
-# 
-# Also note that all of these questions can be answered with one line of code.
 # ____
 # ** Import pandas and read in the Ecommerce Purchases csv file and set it to a DataFrame called ecom. **
 
@@ -139,5 +134,3 @@ sum(ecom['CC Exp Date'].apply(lambda x : x[3:]=='25'))
 
 ecom['Email'].apply(lambda x: x.split('@')[1]).value_counts().head(5)
 
-
-# # Great Job!
